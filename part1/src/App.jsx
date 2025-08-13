18e94816d7ef2fe046e34892ca853670b5d355b9
nicolas misseri <nicolasm@cafar.org.ar>
2025-08-13 15:49:45 -0300

    Renderizado condicional

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,25 @@
 import { useState } from "react"
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join('')}
+    </div>
+  )
+}
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total,setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
@@ -21,7 +37,7 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks}/>
     </div>
   )
 }
