1742860ffab85606c490f4cf678b6c2f292d7288
nicolas misseri <nicolasm@cafar.org.ar>
2025-08-19 15:41:28 -0300

    Falta modularizar cada componente qeu muestra

--- part2/my-app/src/App.jsx
+++ part2/my-app/src/App.jsx
@@ -1,41 +1,73 @@
 import Course from "./components/Course";
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
-
-  const totalExercises = course.parts.reduce((sum,part) => {
-    console.log("cantidad de ejercicios",part.exercises)
-    return sum + part.exercises}, 0)
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   return (
     <div>
-      <h1>{course.name}</h1>
-      <ul>
-        {course.parts.map((part) => (
-          <Course key={part.id} course={part} />
-        ))}
-      </ul>
-      <h2>Total of a exercises: {totalExercises}</h2>
+      <h1>Web development curriculum</h1>
+
+      {courses.map((course) => {
+        const total = course.parts.reduce((sum , part) => sum + part.exercises, 0 );
+
+        return (
+          <div key={course.id}>
+            <h2 key={course.id}>{course.name}</h2>
+            <ul>
+              {course.parts.map((course) => (
+                <li key={course.id}>
+                  {course.name} , {course.exercises}
+                </li>
+              ))}
+
+             <strong>El total de ejercicios es {total}</strong>
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
