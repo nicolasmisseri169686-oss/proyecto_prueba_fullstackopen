70a73ecc00580feb4d3c6ccc51526079522edbe6
nicolas misseri <nicolasm@cafar.org.ar>
2025-09-23 12:39:21 -0300

    Puesta a punto para la migracion a la notebook

--- root/phonebook/src/components/Filter.jsx
+++ root/phonebook/src/components/Filter.jsx
@@ -8,6 +8,13 @@ const Filter = ({ filter, setFilter }) => {
         onChange={(event) => setFilter(event.target.value)}
         placeholder="Search by name (case insensitive)"
       />{" "}
+      <h2>Search Contact</h2>
+      Search Name{" "}
+      <input
+        value={filter}
+        onChange={(event) => setFilter(event.target.value)}
+        placeholder="Search by number (only numbers)"
+      />{" "}
     </div>
   );
 };
